3f810850c9b220c2ef21cd5e0d903084f5f2549f
GeorgiyGiggle <mr.sobaka@gmail.com>
2017-03-07 17:12:06 +0200

    updated HW4

--- HW4/py_hw2.py
+++ HW4/py_hw2.py
@@ -1,15 +1,21 @@
 dict1 = {
-    "first_name": "John",
-    "last_name": "Doe",
-    "email": "johndoe@gmail.com",
-    "age": 25
+    "first_name": "Donald",
+    "last_name": "Trump",
+    "email": "donaldtrump@gmail.com",
+    "age": 39
 }
 
-a = list(dict1.values())
-print(a)
+a = []
 
-for value in dict1.values():
-    if type(value) == str and len(value) > 5:
-        if value.find("a") != -1:
-            a.append()
-    a.append()
+for key in dict1:
+    if(type(dict1[key]) == str and len(dict1[key]) > 5):
+        print(dict1[key]), a.append(dict1[key])
+    if(type(dict1[key]) == int and dict1[key] in range(25, 40)):
+        print(dict1[key]), a.append(dict1[key])
+
+print("More than 5 symbols for strings and range 25-40 for int:", a)
+
+
+for element in a:
+    if type(element) == str and element.find("r") and element.find("m"):
+        a.remove(element), print("Removed words with letters 'R' and 'M':", a)
