7a97c4753a50ed29da448bead8b0b4b4d190f5d9
GeorgiyGiggle <mr.sobaka@gmail.com>
2017-03-23 17:06:22 +0200

    added iterator, generator and list comprehansion

--- HW9/py_hw7.py
+++ HW9/py_hw7.py
@@ -56,3 +56,91 @@ except AttributeError:
     print("Smthng wrong")
     hyperion.__scan_resolution = 500
     print(hyperion.__scan_resolution)
+
+# Iterator, generator & list comprehension
+
+class FileIterator(object):
+    def __init__(self, filename):
+        self.fd = open(filename, 'r')
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        line = self.fd.readline()
+        if line != '':
+            self.line = line
+            return self.line
+        raise StopIteration
+
+
+class ListIterator(object):
+    def __init__(self, items):
+        self.items = items
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            self.item = self.items[self.count]
+            self.count += 1
+            return self.item
+        except IndexError:
+            raise StopIteration
+
+
+list1 = ListIterator(["something", "cool", "is", "right", "here"])
+print(list1.__next__())
+print(list1.__next__())
+print(list1.__next__())
+print(list1.__next__())
+
+
+class DictIterator(object):
+    def __init__(self, items):
+        self.items = items
+
+        self.keylist = []
+        self.vallist = []
+        for key, val in items.items():
+            self.keylist.append(key)
+            self.vallist.append(val)
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            self.item1 = self.keylist[self.count]
+            self.item2 = self.vallist[self.count]
+            self.count += 1
+            return self.item1, self.item2
+        except IndexError:
+            raise StopIteration
+
+dict1 = DictIterator({"cruiser": "rupture",
+                  "battlecruiser": "hurricane",
+                  "battleship": "tempest"})
+print(dict1.__next__())
+print(dict1.__next__())
+print(dict1.__next__())
+
+
+def numbers(first=0, last=100, step=1):
+    number = first
+    while number < last:
+        if number % 3 == 0:
+            yield number
+            number += step
+        else:
+            number += step
+
+item = numbers()
+for num in item:
+    print(num, end=" ")
+
+a = [i for i in range(100) if i % 3 == 0]
+print(a)
